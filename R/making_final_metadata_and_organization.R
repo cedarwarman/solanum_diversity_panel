d62ad1149e3ebb105607f42cbb207cae35625e83
cedarwarman <cedardalewarman@gmail.com>
2021-08-20 11:59:12 -0700

    Consolidate organization scripts

--- R/making_final_metadata_and_organization.R
+++ R/making_final_metadata_and_organization.R
@@ -172,7 +172,26 @@ for (n in seq(1, nrow(razifard))) {
 
 # Adding in the tgrc_varitome data ----------------------------------------
 # This is all well and good, but adding the other names in from the 
-# tgrc_varitome df might reveal some of the dups.
+# tgrc_varitome df to Razifard might reveal some of the dups.
+tgrc_varitome_key <- data.frame(name_TGRC = tgrc_varitome$AccessionNum,
+                                name_BGV = tgrc_varitome$OtherID)
+
+# To deal with later: CATIE-11106/1, PAS014479
+tgrc_varitome_key_extras <- tgrc_varitome_key[c(135,136), ]
+
+# Cleaning up the key for the BGV names
+tgrc_varitome_key <- tgrc_varitome_key[complete.cases(tgrc_varitome_key), ]
+tgrc_varitome_key <- tgrc_varitome_key[str_detect(tgrc_varitome_key$name_BGV, "^BGV"), ]
+
+# Adding the BGV names
+razifard <- razifard %>% 
+  left_join( tgrc_varitome_key, by = c('name_BGV')) %>%
+  mutate(name_TGRC = coalesce(name_TGRC.x, name_TGRC.y), .keep = 'unused') %>%
+  relocate(name_TGRC)
+
+# Manually adding the other two names
+razifard$name_TGRC[razifard$name_other == "CATIE-11106-1"] <- "LA4790"
+razifard$name_TGRC[razifard$name_other == "PAS014479"] <- "LA4791"
 
 
 # Joining -----------------------------------------------------------------
@@ -209,8 +228,8 @@ joined_other <- inner_join(alonge, razifard_name_other, by = "name_other", na_ma
 # Adding the rows that are not shared between the two data frames
 joined <- rbind(joined_pi, joined_bgv, joined_other)
 bind_df <- razifard[! razifard$name_original_razifard %in% joined$name_original_razifard, ]
-
 joined <- bind_rows(joined, bind_df)
+
 bind_df <- alonge[! alonge$name_original_alonge %in% joined$name_original_alonge, ]
 joined <- bind_rows(joined, bind_df)
 
@@ -218,3 +237,168 @@ joined <- bind_rows(joined, bind_df)
 joined <- joined[ , c(1:11, 19, 41, 18, 12:17, 20:40)]
 joined$part_of_alonge[is.na(joined$part_of_alonge)] <- FALSE
 joined$part_of_razifard[is.na(joined$part_of_razifard)] <- FALSE
+
+# Still 236 after adding the TGRC names from tgrc_varitome
+# Checking for duplicates
+# length(joined$name_TGRC[! is.na(joined$name_TGRC)])
+# length(unique(joined$name_TGRC[! is.na(joined$name_TGRC)]))
+# length(joined$name_BGV[! is.na(joined$name_BGV)])
+# length(unique(joined$name_BGV[! is.na(joined$name_BGV)]))
+# length(joined$name_other[! is.na(joined$name_other)])
+# length(unique(joined$name_other[! is.na(joined$name_other)]))
+# No dups in name_TGRC, name_BGV, name_other
+
+# Final Alonge / Razifard dataframe with (hopefully) no dups, all metadata
+accessions <- joined
+
+
+# Manually adding non-long-read/Varitome accessions -----------------------
+# These are accessions that I ordered outside of the main set, mostly things
+# that are from the larger PGRP thermotolerance project.
+to_add <- data.frame("name_TGRC" = c("LA0490",
+                                   "LA1994",
+                                   "LA2375",
+                                   "LA2661",
+                                   "LA2662",
+                                   "LA3120",
+                                   "LA3320",
+                                   "LA4345",
+                                   "LA4355",
+                                   "LA4715",
+                                   "LA4790",
+                                   "LA4792",
+                                   "LA4793"),
+                     "name_CC" = c(NA),
+                     "name_EA" = c(NA),
+                     "name_PI" = c(NA),
+                     "name_TS" = c(NA),
+                     "name_BGV" = c(NA),
+                     "name_FLA" = c(NA),
+                     "name_LYC" = c(NA),
+                     "name_TR" = c(NA),
+                     "name_other" = c("VF-36",
+                                      "Tamaulipas",
+                                      "San Marzano",
+                                      "Nagcarlang",
+                                      "Saladette",
+                                      "Malintka",
+                                      "Hotset",
+                                      "Heinz",
+                                      "Gold Nugget",
+                                      "unknown",
+                                      "unknown",
+                                      "Tegucigalpa",
+                                      "Voyage"),
+                     "name_original_alonge" = c(NA),
+                     "name_original_razifard" = c(NA),
+                     "part_of_razifard" = c(FALSE),
+                     "part_of_alonge" = c(FALSE),
+                     "Species" = c("SLL",
+                                   "SLL",
+                                   "SLL",
+                                   "SLL",
+                                   "SLL",
+                                   "SLL",
+                                   "SLL",
+                                   "SLL",
+                                   "SLL",
+                                   "SP",
+                                   "SLL",
+                                   "SLL",
+                                   "SLL"),
+                     "Taxon" = c(NA),
+                     "SV Count" = c(NA),
+                     "SV Collector Rank" = c(NA),
+                     "SRA ID" = c(NA),
+                     "Yield (bp)" = c(NA),
+                     "Assigned_population" = c(NA),
+                     "Country" = c(NA),
+                     "Province" = c(NA),
+                     "Collecting_locality" = c(NA),
+                     "Local_name" = c(NA),
+                     "Notes_from_original_collections" = c(NA),
+                     "Latitude" = c(NA),
+                     "Longitude" = c(NA),
+                     "fresh_weight_(g)" = c(NA),
+                     "dry_weight_(%)" = c(NA),
+                     "locule_number_(count)" = c(NA),
+                     "pericarp_thickness_(mm)" = c(NA),
+                     "soluble_solids_(Brix)" = c(NA),
+                     "beta-carotene_(mg/g_tissue)" = c(NA),
+                     "lycopene_(mg/g_tissue)" = c(NA),
+                     "citric_acid_(mg/g_tissue)" = c(NA),
+                     "malic_acid_(mg/g_tissue)" = c(NA),
+                     "glucose_(mg/g_tissue)" = c(NA),
+                     "fructose_(mg/g_tissue)" = c(NA),
+                     "alpha-tomatine_(mg/g_tissue)" = c(NA),
+                     "dehydrotomatine_(mg/g_tissue)" = c(NA))
+
+# The slashes and parentheses don't make it to colnames, but this seems to 
+# fix it:
+colnames(to_add) <- colnames(accessions)
+
+# Combining them with the larger set:
+accessions <- rbind(accessions, to_add)
+
+
+# Adding consistent species metadata --------------------------------------
+# I want to be able to sort by species, so I'll add a column and make the 
+# names consistent.
+colnames(accessions)[15] <- "alonge_species" # "species" was taken
+accessions$species <- NA
+accessions <- accessions[ , c(1:14, 42, 15:41)]# fixing order
+
+accessions$species[grepl("SP", accessions$Assigned_population)] <- "pimpinellifolium"
+accessions$species[grepl("SLL", accessions$Assigned_population) | 
+                           grepl("SLC", accessions$Assigned_population)] <- "lycopersicum"
+accessions$species[grepl("SP",accessions$alonge_species)] <- "pimpinellifolium"
+accessions$species[grepl("SLL", accessions$alonge_species)] <- "lycopersicum"
+accessions$species[grepl("SLC", accessions$alonge_species)] <- "lycopersicum"
+accessions$species[grepl("GAL", accessions$alonge_species)] <- "galapagense"
+accessions$species[grepl("CHE", accessions$alonge_species)] <- "cheesmaniae"
+accessions$species[accessions$name_TGRC == "LA4768"] <- "lycopersicum" # species from TGRC
+accessions$species[accessions$name_BGV == "BGV006148"] <- "pimpinellifolium" # species from TGRC, intermediate
+accessions$species[accessions$name_TGRC == "LA0767"] <- "lycopersicum" # species from TGRC
+
+# Put this packet aside: CATIE-11106-1
+# accessions <- accessions[accessions$accession != "CATIE-11106-1", ]
+
+# Removing an accession that wasn't sent 
+accessions <- accessions[is.na(accessions$name_other) | accessions$name_other != "Voyage", ]
+
+# CATIE and Voyage are problems. What happened with CATIE again?? Removing 
+# CATIE for now, but check.
+accessions <- accessions[is.na(accessions$name_other) | accessions$name_other != "CATIE-11106-1", ]
+
+# Problems
+nrow(accessions[is.na(accessions$species), ]) # Should be none
+
+
+# Adding seed packet information ------------------------------------------
+# Sara put together a list of seed packets. I will add this information to 
+# the table of accessions from Alonge and Razifard.
+
+gs4_deauth()
+packet_list <- read_sheet("1RMDwiJy14HxPH7kY3pwske3iXLqL8N3WLHIl2o0WQW8")
+packet_list <- packet_list[ , 1]
+colnames(packet_list) <- c("packet_name")
+
+# Go through packet names, check all the name columns, if match add the 
+# packet name to a new packet name column (make two), if first is full 
+# then add to the second one, print error if the second on is full too. 
+# at end, mark dups by checking packet name columns.
+
+# Adding packet columns
+accessions <- accessions %>% 
+  mutate(packet_name_1 = NA,
+         packet_name_2 = NA) %>%
+  relocate(c(packet_name_1, packet_name_2))
+
+
+
+
+
+
+
+
+
